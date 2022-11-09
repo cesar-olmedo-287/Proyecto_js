d3588d561ea8ddec1845622d84d07baafc29e1f5
cesarolmedo <cesitar287@hotmail.com>
2022-11-08 23:17:28 -0300

    segunda entrega

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ let nombre = prompt(`ingresa tu nombre`)
 alert(`Bienvenido ${nombre}`)
 let comprar = true
 let total = 0
+class Producto {
+    constructor(id, nombre, precio, stock){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
+const remera = new Producto (1, "Remera", 1000, 20);
+const jean = new Producto (2, "Jean", 2500, 10);
+const camisa = new Producto (3, "Camisa", 1500, 15);
+const medias = new Producto (4, "Medias", 300, 50);
+const productos = [remera, jean, camisa, medias];
+
 let producto = parseInt(prompt(`Deseas comprar: 1.Remera, 2.Jean, 3.Camisa, 4.Medias`))
 while (comprar === true){
-    if(producto === 1){
-        total = total + 1000
-    }else if (producto === 2){
-        total = total + 2500
-    }else if (producto === 3){
-        total = total + 1500
-    }else if (producto === 4){
-        total = total + 300
+    if(producto === remera.id){
+        total = total + remera.precio
+        remera.stock = remera.stock - 1
+    }else if (producto === jean.id){
+        total = total + jean.precio
+        jean.stock = jean.stock - 1
+    }else if (producto === camisa.id){
+        total = total + camisa.precio
+        camisa.stock = camisa.stock - 1
+    }else if (producto === medias.id){
+        total = total + medias.precio
+        medias.stock = medias.stock - 1
     }
 let seguirComprando =parseInt(prompt(`¿Deseas agregar algo más?: 1.Si, 2.No`))
 if(seguirComprando === 1){
@@ -31,3 +49,8 @@ function descuento(total){
 }
 let totalConDescuento = descuento(total)
 alert(`${nombre} el total de tu compra es de $${totalConDescuento}`)
+for (let index = 0; index < 4; index++){
+    console.log(productos[index].nombre);
+    console.log(productos[index].stock);
+}
+
